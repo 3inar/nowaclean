d3a14555820cb7f20be206cfef6500f862ba57d2
Einar Holsbø <einar@cs.uit.no>
2016-08-18 16:01:34 +0200

    add rowname requirement for dens()

--- tests/testthat/test_dens.R
+++ tests/testthat/test_dens.R
@@ -1,5 +1,6 @@
 testdata <- matrix(c(rnorm(100*100, sd=2), rnorm(100*100, sd=5),
                   rnorm(100*100, sd=3), rnorm(100*100, sd=7)), nrow = 100)
+rownames(testdata) <- as.character(1:nrow(testdata))
 obj <- dens(testdata)
 
 test_that("dens() returns object of class dens", {
@@ -10,4 +11,11 @@ test_that("plot takes optional highlight= arg of character vector of rownames", 
   pdf(file=NULL) # discard plot
   expect_error(plot(obj, highlight=c(1,2,3)))
   dev.off()
+})
+
+test_that("dens() throws error if no rownames exist", {
+  d <- testdata
+  rownames(d) <- NULL
+
+  expect_error(dens(d))
 })
