e4813ed0d22c6e8a4fa56effeda33efb4a480247
Einar Holsbø <einar@cs.uit.no>
2016-08-30 16:23:13 +0200

    Make lab measures more explicit.

--- R/lab_measures.R
+++ R/lab_measures.R
@@ -1,6 +1,21 @@
+#' Grounds for excluding a sample based on lab measures
+#'
+#' We have several quality control measurements from the lab; when we are
+#' uncertain whether an array looks bad we can inspect these measures and
+#' get a sense of whether something has gone wrong. We use these cutoffs to
+#' say that an array has gone wrong:
+#' \itemize{
+#'  \item{\strong{RIN < 7:}}{ An array with RIN less than 7 is considered bad.}
+#'  \item{\strong{260/280 RNA ratio < 2:}}{ An array with 260/280 less than 2 is considered bad.}
+#'  \item{\strong{260/230 RNA ratio < 1.7:}}{ An array with 260/230 less than 1.7 is considered bad.}
+#'  \item{\strong{Ng/ul RNA outside of (50, 500):}}{ Well-behaved arrays should have
+#'  50 < Ng/ul RNA < 500.}
+#' }
+#' \strong{NB} that perfectly well-behaved arrays might have lab measures outside
+#' of these limits, we never exclude arrays based only on lab measures.
 #' @export
-lab_thresholds <- c("RIN value < 7", "260/280 ratio < 2",
-                    "260/230 ratio < 1.7", "50 < RNA < 500")
+lab_thresholds <- c("Bad: RIN value < 7", "Bad: 260/280 RNA ratio < 2",
+                    "Bad: 260/230 RNA ratio < 1.7", "Good: 50 < Ng/ul RNA < 500")
 
 #' @export
 lab_variables <- c("Ng/ul_RNA", "260/280_RNA", "260/230_RNA", "RIN",
