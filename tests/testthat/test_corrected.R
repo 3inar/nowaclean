4d1a5079d8eb13ed9027c20d5ae1e00fb154320f
Einar Holsbø <einar@cs.uit.no>
2016-12-08 20:42:18 +0100

    skip tests if missing packages

--- tests/testthat/test_corrected.R
+++ tests/testthat/test_corrected.R
@@ -1,12 +1,12 @@
 testdata <- matrix(c(rnorm(100, mean=10, sd=1),
                      rnorm(100, mean=10, sd=2),
-                     rnorm(100, mean=10, sd=3), 
-                     rnorm(100, mean=10, sd=4)), 
+                     rnorm(100, mean=10, sd=3),
+                     rnorm(100, mean=10, sd=4)),
                      nrow = 100)
 
 negs <- cbind(testdata, matrix(c(rnorm(100, mean=10, sd=1),
                  rnorm(100, mean=10, sd=2)),
-                 nrow=100)) 
+                 nrow=100))
 
 colnames(testdata) <- c("V1","V2","V3","V4")
 colnames(negs) <- c(colnames(testdata), "V5", "V6")
@@ -17,14 +17,26 @@ colnames(testdata_with_zeros) <- c("V1","V2","V3","V4","zeros")
 testdata_with_negative <- cbind(testdata, rnorm(100,sd=1))
 colnames(testdata_with_negative) <- c("V1","V2","V3","V4","negative")
 
+suggested_pkgs <- c(
+  "lumi",
+  "limma",
+  "preprocessCore",
+  "AnnotationDbi",
+  "lumiHumanAll.db",
+  "illuminaHumanv4.db"
+)
+
 test_that("corrected() works when all values are positive", {
+  plyr::a_ply(suggested_pkgs, 1, skip_if_not_installed)
   expect_output(corrected(testdata, negs))
 })
 
 test_that("corrected() throws an error when one column is all zeros", {
+  plyr::a_ply(suggested_pkgs, 1, skip_if_not_installed)
   expect_error(corrected(testdata_with_zeros, negs))
 })
 
 test_that("corrected() throws an error when one column has negative values",{
+  plyr::a_ply(suggested_pkgs, 1, skip_if_not_installed)
   expect_error(corrected(testdata_with_negative, negs))
 })
