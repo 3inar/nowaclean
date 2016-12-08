1a5743b89000c23ea34782d73a760a8e4700ad10
Einar Holsbø <einar@cs.uit.no>
2016-12-08 20:08:04 +0100

    make preprocessing req's less severe

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,45 +1,70 @@
+req_pkg <- function(pkg_name) {
+  if (!requireNamespace(pkg_name, quietly = TRUE)) {
+    stop(pkg_name, " (biconductor) needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+}
+
+req_pkgs <- function() {
+  package_list <- c(
+    "lumi",
+    "limma",
+    "preprocessCore",
+    "AnnotationDbi",
+    "lumiHumanAll.db",
+    "illuminaHumanv4.db"
+  )
+
+  plyr::a_ply(package_list, 1, req_pkg)
+}
+
+
+
+
 # do clean 1st
 #' @export
 corrected <- function(data, negative_controls) {
-  
+  req_pkgs()
+
   if(is(data, "LumiBatch")){
     expr <- Biobase::exprs(data)
-  } else if(is(data, "matrix")) { 
+  } else if(is(data, "matrix")) {
     expr <- data
   } else {
     stop("data must be either a LumiBatch object or a matrix")
   }
-  
+
   no_variance = names(which(apply(expr, 2, sd) == 0))
   if(length(no_variance > 0)){
     stop("Found samples with no variance (e.g. gene expression values all zero). Please investigate samples: ",
          paste(unlist(no_variance), collapse=" "), ".")
   }
-  
+
   negative = names(which(apply(expr, 2, min) < 0))
   if(length(negative > 0)){
     stop("Found samples with negative gene expression values. Please investigate samples: ",
          paste(unlist(negative), collapse=""), ".")
   }
-  
-  
+
+
   total <- rbind(expr, negative_controls[, colnames(expr)])
   probe_t <- c(rep("regular", nrow(expr)), rep("negative", nrow(negative_controls)))
   expr <- limma::nec(total, probe_t)
   expr <- expr[probe_t == "regular", ]
-  
+
   if(is(data, "LumiBatch")){
     Biobase::exprs(data) <- expr
   } else {
     data <- expr
   }
-  
+
   data
 }
 
 
 #' @export
 normalized <- function(data) {
+  req_pkgs()
   tmp <- preprocessCore::normalize.quantiles(exprs(data), copy=TRUE)
   rownames(tmp) <- rownames(data)
   colnames(tmp) <- colnames(data)
@@ -53,48 +78,51 @@ normalized <- function(data) {
 # also filters bad probes
 #' @export
 filtered <- function(data, pval=0.01, fval=0.01, verbose=FALSE) {
+  req_pkgs()
   threshold <- round(fval*ncol(data))
 
   # counts number of samples with probe detectable at pval level
   present <- lumi::detectionCall(data, Th=pval, type="probe")
   remove <- present < threshold
-  
+
   probeq <- illuminaHumanv4.db::illuminaHumanv4PROBEQUALITY
   probes <- lumi::nuID2IlluminaID(rownames(data), lib.mapping=NULL,
                                   species ="Human", idType='Probe')
   probe_quality <- unlist(AnnotationDbi::mget(as.character(probes), probeq, ifnotfound=NA))
   bad_quality <- (probe_quality == "Bad") | (probe_quality == "No match")
-  
+
   if(verbose){
-    cat("Removing", sum(remove), "probes that were not present in at least", 
+    cat("Removing", sum(remove), "probes that were not present in at least",
         fval*100, "% of the samples.\n")
     cat("Removing", sum(bad_quality), "bad quality probes.\n")
   }
-  
+
   data <- data[-which(remove | bad_quality), ]
   data
 }
 
 #' @export
 probe_aggregated <- function(data, verbose=FALSE) {
+  req_pkgs()
   probes_in = length(Biobase::featureNames(data))
-  
+
   annotation(data) <- "lumiHumanAll"
   data <- genefilter::nsFilter(data, var.filter=FALSE)$eset ## aggregate across annotated probes
-  
+
   probes_out = length(Biobase::featureNames(data))
   if(verbose){
     cat("Filtered out", probes_in-probes_out,
       "probes.\n")
   }
-  
+
   data
 }
 
 #' @export
 gene_names <- function(data) {
+  req_pkgs()
   nu_ids <- rownames(data)
-  mapping <- nuID2RefSeqID(nu_ids, lib.mapping='lumiHumanIDMapping', returnAllInfo =TRUE)
+  mapping <- lumi::nuID2RefSeqID(nu_ids, lib.mapping='lumiHumanIDMapping', returnAllInfo =TRUE)
 
   mapping
 }
